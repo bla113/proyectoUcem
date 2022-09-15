9dad0d74ed432a010f94a16e9a4e5bbe03466d49
bla113 <bla_113@hotmail.com>
2022-09-15 14:02:18 -0600

    Cambios Significativos

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
   <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <link href="css/style.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
+
 
 
   <!-- Custom styles for this template -->
@@ -23,12 +25,11 @@
       <div class="container-fluid">
 
         <img src="img/logoUcem.png" alt="LogoUcem" width="120">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-          aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <ul class="navbar-nav me-auto mb-4 mb-md-0">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Nosotros</a>
             </li>
@@ -39,7 +40,7 @@
               <a class="nav-link active " aria-current="page" href="pages/eventos.php">Eventos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active " aria-current="page" href="pages/noticias.php">Noticias</a>
+              <a class="nav-link active " aria-current="page" href="pages/blog.php">Noticias</a>
             </li>
             <li class="nav-item">
               <a class="nav-link active " aria-current="page" href="pages/biblioteca.php">Biblioteca</a>
@@ -48,10 +49,13 @@
               <a class="nav-link active " aria-current="page" href="pages/recursos.php">Recursos Académicos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active " aria-current="page" href="pages/solicitudes.php">Solicitudes</a>
+              <a class="nav-link active " aria-current="page" href="pages/convalidaciones.php">Convalidaciones</a>
             </li>
             <li class="nav-item">
               <a class="nav-link active " aria-current="page" href="pages/login.php">Ingreso al Sistema</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active " aria-current="page" href="pages/frmCrearCuenta.php">Crear Cuenta</a>
             </li>
 
           </ul>
@@ -72,20 +76,18 @@
 
     <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
-          aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active">
 
-          <img class="bd-placeholder-img" width="100%" height="100%" src="img/Estudiante6.jpg" alt="" aria-hidden="true"
-            preserveAspectRatio="xMidYMid slice" focusable="false">
+          <img class="bd-placeholder-img" width="100%" height="100%" src="img/Estudiante6.jpg" alt="" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
           <rect width="100%" height="100%" fill="#777" />
           <div class="container">
             <div class="carousel-caption text-start">
-              <h1 class="display-5 fw-bold text-primary"  >Conocimiento a tu alcance</h1>
+              <h1 class="display-5 fw-bold text-primary">Conocimiento a tu alcance</h1>
               <p>Contruya su futuro con la Universidad de las Ciencias Empresariales</p>
               <p><a class="btn btn-lg btn-primary" href="pages/frmMatricula.php">Matricular</a></p>
             </div>
@@ -93,8 +95,7 @@
         </div>
         <div class="carousel-item">
 
-          <img class="bd-placeholder-img" width="100%" height="100%" src="img/Graduacion1.jpg" alt="" aria-hidden="true"
-            preserveAspectRatio="xMidYMid slice" focusable="false">
+          <img class="bd-placeholder-img" width="100%" height="100%" src="img/Graduacion1.jpg" alt="" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
 
           <div class="container">
             <div class="carousel-caption">
@@ -105,8 +106,7 @@
           </div>
         </div>
         <div class="carousel-item">
-          <img class="bd-placeholder-img" width="100%" height="100%" src="img/Ingeniero2.jpg" alt="" aria-hidden="true"
-            preserveAspectRatio="xMidYMid slice" focusable="false">
+          <img class="bd-placeholder-img" width="100%" height="100%" src="img/Ingeniero2.jpg" alt="" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
 
           <div class="container">
             <div class="carousel-caption text-end">
@@ -138,8 +138,7 @@
       <div class="row">
         <div class="col-lg-4">
 
-          <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="img/IngenieririaIndustria2.jpg"
-            alt="">
+          <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="img/IngenieririaIndustria2.jpg" alt="">
 
           <h2 class="fw-normal">Ingenierias</h2>
           <p>Las Ingenierias con más alata demanda Laboral de pais.</p>
@@ -152,11 +151,10 @@
           <h2 class="fw-normal">Progrmas Técnicos</h2>
           <p>Programas Técnicos de Python, JavaScript,HTML5, con mas demandas laboral.
           </p>
-          <p><a class="btn btn-secondary" href="pages/frmMatricula.php">View details &raquo;</a></p>
+          <p><a class="btn btn-secondary" href="pages/frmMatricula.php">Matricular &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="img/ingeniroindustrial.jpg"
-            alt="">
+          <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="img/ingeniroindustrial.jpg" alt="">
 
           <h2 class="fw-normal">Facultades y Programas</h2>
           <p>Más de 6 carreras disponibles para que te formés como profesional en la Universidad Privada más
@@ -183,70 +181,81 @@
             <p class="col-md-8 fs-4">Soy el director académico de la Universidad UCEM Alajuela y me gustaría compartir con ustedes un minuto de su tiempo.
 
               Al igual que ustedes, también tuve que estar sentado en las aulas universitarias por cuatro o más años para poder obtener un grado académico que me permitiera insertarme en el mercado laboral y así mejorar mis condiciones de vida para mí y mi familia.
-              
+
               También tengo claridad de todos los sacrificios que durante ese tiempo tuvieron y tendrán que hacer, estar lejos de la familia, no atender a sus seres queridos, no disfrutar de momentos gratos con amigos, ¿con qué fin?
-              
+
               El mundo es un lugar bello, pero muy complejo, donde la preparación académica tiene un lugar importantísimo en la gestión de oportunidades laborales, permitiéndonos crecer paulatinamente y escalar mejores posiciones para nosotros y por ende a nuestras familias.
-              
+
               Como les dije a los que conozco, yo no miento, y no voy a decir que completar una carrera universitaria es sencillo o fácil. Siempre buscaremos las opciones para mejorar sus habilidades y conocimientos, independientemente que estas acciones no sean del agrado de todas las personas.
-              
+
               Por ello, los insto a continuar con sus estudios en la universidad, a hacerle frente a los retos que representa llamarse ESTUDIANTE UNIVERSITARIO, y así, con un esfuerzo conjunto entre la universidad y ustedes considero que es posible que podamos convertirnos en esos profesionales que hemos anhelado siempre ser.
-              
+
               Les agradezco su tiempo y que Dios los bendiga.
-              
-              </p>
-              <h2>Quedo a sus órdenes</h2>
-            
+
+            </p>
+            <h2>Quedo a sus órdenes</h2>
+
           </div>
         </div>
 
         <div class="row align-items-md-stretch">
           <div class="col-md-6">
             <div class="h-100 p-5 text-bg-dark rounded-3">
-              <h2>Change the background</h2>
-              <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then,
-                mix and match with additional component themes and more.</p>
-              <button class="btn btn-outline-light" type="button">Example button</button>
+              <h2>Contáctenos</h2>
+              <h4><i class="fa-solid fa-phone"></i> Teléfono:(+506) 4070-2035 </h4>
+              <h4><i class="fa-brands fa-whatsapp"></i> Whatsapp:(+506) 8344-6379 </h4>
+              <h4><i class="fa-regular fa-envelope-open"></i> Correo Electrónico: academico@ucem.ac.cr </h4>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d245.56058221424877!2d-84.21896650957916!3d10.019390766539168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0f9cfd8af7a55%3A0xc33018aca571087a!2sUniversidad%20UCEM!5e0!3m2!1ses-419!2scr!4v1663271703008!5m2!1ses-419!2scr" width="500" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
           </div>
           <div class="col-md-6">
             <div class="h-100 p-5 bg-light border rounded-3">
-              <h2>Add borders</h2>
-              <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure
-                to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both
-                column's content for equal-height.</p>
-              <button class="btn btn-outline-secondary" type="button">Example button</button>
+              <h2>Noticias</h2>
+              <div class="col">
+                <div class="card shadow-sm">
+
+                  <img src="img/Banner-CAmbio.png" alt="" class="bd-placeholder-img card-img-top" width="100%" height="250">
+                  <div class="card-body">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Anterior</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Siguiente</button>
+                      </div>
+                      <small class="text-muted">2 Días</small>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
             </div>
           </div>
+
+
         </div>
-
-
-    </div>
-  </main>
+      </main>
 
 
 
-  <!-- /END THE FEATURETTES -->
+      <!-- /END THE FEATURETTES -->
 
-  </div><!-- /.container -->
+    </div><!-- /.container -->
 
-  <hr class="featurette-divider">
-  <!-- FOOTER -->
+    <hr class="featurette-divider">
+    <!-- FOOTER -->
 
-  <div class="bg-info text-secondary px-4 py-5 text-center">
-    <div class="py-5">
-      <h1 class="display-5 fw-bold text-white">Universidad de las Ciencias Empresariales</h1>
-      <div class="col-lg-6 mx-auto">
-        <p class="fs-5 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
-          popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive
-          prebuilt components, and powerful JavaScript plugins.</p>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
-          <button type="button" class="btn btn-outline-light btn-lg px-4">Secondary</button>
+    <div class="bg-info text-secondary px-4 py-5 text-center">
+      <div class="py-5">
+        <h1 class="display-5 fw-bold text-white">Universidad de las Ciencias Empresariales</h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="fs-5 mb-4">Todos los derechos Reservados 2022</p>
+          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+           
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
 
   </main>
