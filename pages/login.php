7140238095ac698176a20a0175f3f4ac761064c1
bla113 <bla_113@hotmail.com>
2022-09-15 12:34:46 -0600

    Maquetación
    Inicio del Proyecto

---
+++ pages/login.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.101.0">
+  <title>Ingreso Estudiantes</title>
+
+
+  <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/style.css" rel="stylesheet">
+  
+  <!-- Hoja de Estilos Css -->
+  <link href="../css/signin.css" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/fotawesome/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+
+
+</head>
+
+<body class="text-center">
+
+  <main class="form-signin w-100 m-auto">
+    <form>
+      <img class="mb-4" src="../img/logoUcem.png" alt="LogoUcem" width="72" height="57">
+      <h1 class="h3 mb-3 fw-normal">Ingrese Credenciales</h1>
+
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Correo Electrónico</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña">
+        <label for="floatingPassword">Contraseña</label>
+      </div>
+
+      <div class="checkbox mb-3">
+       
+        
+      <i class="fa-solid fa-address-card"></i>
+        <a href="../pages/frmCrearCuenta.php">Crear Cuenta</a>
+        
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Ingresar</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
+    </form>
+  </main>
+
+
+
+</body>
+
+</html>
